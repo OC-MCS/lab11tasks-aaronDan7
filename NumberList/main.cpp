0d1f19d05a81be1eb2ad6294d0e30f9076a20bf4
Aaron Hill <aaron.hill@eagles.oc.edu>
2019-04-07 22:00:43 -0500

    attempt

--- NumberList/main.cpp
+++ NumberList/main.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include "NumberList.h"
 using namespace std;
 
 int main()
 {
-	 
+	NumberList list;
+
+	list.insertNode(10);
+	list.appendNode(40);
+	list.insertNode(30);
+	list.insertNode(25);
+	list.deleteNode(30);
+
+	list.displayList;
+
+	fstream file;
+	file.open("numbers.txt");
+
+	while (file)
+	{
+		int i;
+		file >> i;
+		list.insertNode;
+	}
+	list.displayList;
+
+
 	return 0;
 }
